fe036c2ea42da02d7e868d65b8e3ca4e5a47c8ca
Vassily <vasil.deville@gmail.com>
2018-04-09 14:44:49 +0300

    Fix mistakes

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -18,7 +18,7 @@ def _get_latest_source():
     current_commit=local("git log -n 1 --format=%H", capture=True)
     run(f'git reset --hard {current_commit}')
 
-def _update_database():
+def _update_virtualenv():
     '''Make sure everything is installed'''
     if not exists('virtualenv/bin/pip'):
         run(f'python3.6 -m venv virtualenv')
