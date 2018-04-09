2ae3cfba8b6074e3189a1b71d7bb2950a992f2ee
Vassily <vasil.deville@gmail.com>
2018-04-09 14:38:35 +0300

    Add the fabfile and the info

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,55 @@
+import random
+
+from fabric.contrib.files import append, exists
+from fabric.api import cd, env, local, run
+
+
+REPO_URL = 'https://github.com/vassily-la/obey01.git'
+
+
+def _get_latest_source():
+    '''Do a git clone for fresh deploys.
+    Do git fetch + git reset --hard, if already cloned.
+    Force overwriting any local changes'''
+    if exists('.git'):
+        run('git fetch')
+    else:
+        run (f'git clone {REPO_URL}')
+    current_commit=local("git log -n 1 --format=%H", capture=True)
+    run(f'git reset --hard {current_commit}')
+
+def _update_database():
+    '''Make sure everything is installed'''
+    if not exists('virtualenv/bin/pip'):
+        run(f'python3.6 -m venv virtualenv')
+    run('./virtualenv/bin/pip install -r reqs.txt')
+
+def _create_or_update_dotenv():
+    '''Create or update the .env
+    Provide the secret key, if none'''
+    append('.env', 'DJANGO_DEBUG_FALSE=y')
+    append('.env', f'SITENAME={env.host}')
+    current_contents = run('cat .env')
+    if 'DJANGO_SECRET_KEY' not in current_contents:
+        new_secret = ''.join(random.SystemRandom().choices(
+            'abcdefghijklmnopqrstuvwxyz0123456789', k=50
+        ))
+        append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+
+def _update_static_files():
+    '''Collect static'''
+    run('./virtualenv/bin/python manage.py collectstatic --noinput')
+
+def _update_database():
+    '''Migrate the DB, if necessary'''
+    run('./virtualenv/bin/python manage.py migrate --noinput')
+
+def deploy():
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    run(f'mkdir -p {site_folder}')
+    with cd(site_folder):
+        _get_latest_source()
+        _update_virtualenv()
+        _create_or_update_dotenv()
+        _update_static_files()
+        _update_database()
